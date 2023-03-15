33935f20b0a3302b62a0cddaaf4bb92352417de3
sanzeev <memyselfsd11@gmail.com>
2023-03-15 22:35:10 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Banner/Header";
 import ProfComponent from "./components/Banner/ProfComponent";
 import Clients from "./components/Clients/Clients";
 import Footer from "./components/Footer/Footer";
-import Projects from "./components/Projects/Projects";
 import Services from "./components/Service/Services";
 function App() {
   return (
